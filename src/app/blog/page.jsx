f229cfcda6610b5c74828a1683d97769ba876a2e
Halkano Malise <kudezx@gmail.com>
2023-07-29 17:31:46 +0300

    updated

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
 import style from "./page.module.css";
+import Image from "next/image";
 
 const Blog = () => {
-  return <div className={style.container}>Blog</div>;
+    return (
+        <div className={style.container}>
+            <Link href="/testId" className={style.container}>
+                <div className={style.container}>
+                    <Image
+                        src="https://images.pexels.com/photos/417826/pexels-photo-417826.jpeg"
+                        alt=""
+                        width={400}
+                        height={250}
+                        className={style.image}
+                    />
+                </div>
+                <div className={style.content}>
+                    <h1 className={style.title}>Test</h1>
+                    <p className={style.desc}>Description</p>
+                </div>
+            </Link>
+        </div>
+    );
 };
 
 export default Blog;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -21,6 +21,38 @@ const Category = ({ params }) => {
                     />
                 </div>
             </div>
+
+            <div className={style.item}>
+                <div className={style.content}>
+                    <h1 className={style.title}>Test</h1>
+                    <p className={style.desc}>Desc</p>
+                    <Button text="See More" url="#" />
+                </div>
+                <div className={style.imgContainer}>
+                    <Image
+                        className={style.img}
+                        fill={true}
+                        src="https://images.pexels.com/photos/460736/pexels-photo-460736.jpeg"
+                        alt="sculpture"
+                    />
+                </div>
+            </div>
+
+            <div className={style.item}>
+                <div className={style.content}>
+                    <h1 className={style.title}>Test</h1>
+                    <p className={style.desc}>Desc</p>
+                    <Button text="See More" url="#" />
+                </div>
+                <div className={style.imgContainer}>
+                    <Image
+                        className={style.img}
+                        fill={true}
+                        src="https://images.pexels.com/photos/460736/pexels-photo-460736.jpeg"
+                        alt="sculpture"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
