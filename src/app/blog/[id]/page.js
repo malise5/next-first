35939b22bf6b16cd7704347ac8283835e6e51204
Halkano Malise <kudezx@gmail.com>
2023-07-29 17:59:27 +0300

    enabled

--- src/app/blog/[id]/page.js
+++
@@ -1,5 +0,0 @@
-const BlogPost = () => {
-  return <div>BlogPost</div>;
-};
-
-export default BlogPost;
---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import style from "./page.module.css";
+const BlogPost = () => {
+    return (
+        <div className={style.container}>
+            <div className={style.top}>
+                <div className={style.info}>
+                    <h1 className={style.title}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                    </h1>
+                    <p className={style.desc}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Optio aliquid excepturi vero, quis at magni nihil
+                        sint commodi dolor rerum!Optio aliquid excepturi vero,
+                        quis at magni nihil sint commodi dolor rerum!
+                    </p>
+                    <div className={style.author}>
+                        <Image
+                            src="https://images.pexels.com/photos/1108572/pexels-photo-1108572.jpeg"
+                            alt=""
+                            width={40}
+                            height={40}
+                            className={style.avatar}
+                        />
+                        <span className={style.username}>Kudez</span>
+                    </div>
+                </div>
+                <div className={style.imageContainer}>
+                    <Image
+                        src="https://images.pexels.com/photos/1108572/pexels-photo-1108572.jpeg"
+                        alt=""
+                        fill={true}
+                        className={style.image}
+                    />
+                </div>
+            </div>
+            <div className={style.content}>
+                <p className={style.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+                    esse hic natus fugiat ducimus impedit, atque incidunt porro
+                    consectetur! Saepe totam, accusamus dolores doloribus ut
+                    <br />
+                    <br />
+                    quod ad assumenda magni natus. Ipsam soluta aliquam
+                    cupiditate commodi rem ratione accusantium iure quam at in
+                    <br />
+                    <br />
+                    consequatur reprehenderit aperiam optio doloribus earum,
+                    laborum necessitatibus, sit delectus nemo quibusdam illo quo
+                    perspiciatis voluptas. Repudiandae, voluptas.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default BlogPost;
