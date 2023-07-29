4ffd855cb0260d623e91e49b54e15da1681f9704
Halkano Malise <kudezx@gmail.com>
2023-07-29 21:43:51 +0300

    updated

--- src/components/DarkMode/Toggle.jsx
+++ src/components/DarkMode/Toggle.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
 import style from "./toggle.module.css";
+import { ThemeContext } from "../../../context/ThemeContext";
 
 const Toggle = () => {
+    const { toggle, mode } = useContext(ThemeContext);
     return (
-        <div className={style.container}>
+        <div className={style.container} onClick={toggle}>
             <div className={style.icon}>🌙 </div>
             <div className={style.icon}>🌞 </div>
-            <div className={style.ball} />
+            <div
+                className={style.ball}
+                style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+            />
         </div>
     );
 };
